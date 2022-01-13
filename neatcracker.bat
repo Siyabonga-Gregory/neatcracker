e23048faf47cb8821881901a9fe74e25380675a1
Siyabonga Gregory <huge.fuze@gmail.com>
2022-01-13 03:32:38 +0200

    First Commit

---
+++ neatcracker.bat
@@ -0,0 +1,71 @@
+@echo off
+color 0a
+:start
+echo ddddddddddddddddddddddddddddddddmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmm
+echo ddddddddddddddddddddddddddddddmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmm
+echo dddddddddddddddddddddddddddmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmNmmmmmmmmmmmmm
+echo ddddddddddddddddddddddddddmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmmmmmm
+echo ddddddddddddddddddddddddmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmmmmm
+echo ddddddddddddddddddddmmmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmmm
+echo dddddddddddddddddddmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmmm
+echo ddddddddddddddddddmmNNNNNNNNNNNNNNNNNmmmmmmmmmmmmmNmdddmmNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmmm
+echo dddddddddddddddddmNNNNNNNNNNNNNNNNmmddhhhhhhyyyyyhdddhyyhdmmdmNmmmmNNNNNNNNNNNNNNNNNNNNNmmmmmmmmmmmm
+echo dddddddddddddddmmNNNNNNNNNNNNNNNmdhyyysooo++////++oyysoooshddmmmdhhhhdmNNNNNNNNNNNNNNNNNNmmmmmmmmmmm
+echo ddddddddddddddmmNNNNNNNNNNNNNNmhyyssoooo++///////://+/////+syyhhyysooyddmmmmNNNNNNNNNNNNNNNmmmmmmmmm
+echo dddddddddddddmmNNNNNNNNNNNNNNdysoooooo+++////:::::::::::::/++o+++///+ossyhhhdmNNNNNNNNNNNNNmmmmmmmmm
+echo ddmmdddddddddmmNNNNNNNNNNNNNdysooo+++++++///:::::::::::::::::::::::::///+++osyhmNNNNNNNNNNNmmmmmmmmm
+echo mmmddddddddmmmNNNNNNNNNNNNNmhyso+++++++++////:::::::::::-:::::::::::://///+++oshmNNNNNNNNNNNmmmmmmmm
+echo mmmdddddddmmmmNNNNNNNNNNNNNmdyss+++++////////:::::::::::::::::::::::://///+++ooyhmNNNNNNNNNNNmmmmmmm
+echo mmmmddmdddmmmmNNNNNNNNNNNNNmdysso+++////+////::://::::::::::::::::::://///+++oosyhmNNNNNNNNNNmmmmmmm
+echo mmmmmmmmmmmmmNNNNNNNNNNNNNNNdysso+++//////::::::///::::::::::::::::::////++++oosssymNNNNNNNNNmmmmmmm
+echo mmmmmmmmmmmmmmNNNNNNNNNNNNNmhysso++//////:::::::::::::::--:::::::::::////+++++oosssdNNNNNNNNNmmmmmmd
+echo mmmmmmmmmmmmmmmNNNNNNMNNNNNmhysoo+///:::::::::::::::-------::::::::::///+++++oooosydNNNNNNNNmmmmmmmm
+echo mmmmmmmmmmmmmmmNNNNNNNNNNNmhysoo+++++/::::::///:::::-----:--::::--::::///++++oososymNNNNNNNNmmmmmmmm
+echo mmmmmmmmmmmmmmmNNNNNNMNNNNhyysooossoo++++++++//+///:::::::---:::::-::/+///++++oossymNNNNNNNNmmmmmmmd
+echo mmmmmmmmmmmmmmNNNNNNMNNNNdyyssssyysossssyhhhhyssoo++//::::::::::::::/++///+++++osshmNNNNNNNNmmmmdddd
+echo mmmmmmmmmmmmmmmNNNNMNNNNmhyssyysssoooooooosyhdhhhyso++///::://///++++++++////++oosyNNNNNNNNmmmmmmmdd
+echo mmmmmmmmmmmmmmmmNNNNNNNmhysssssssssoooooooosyyyyyhyyso+++///+++oossyyssysoo+oossssymNNNNNNmmmmdmdddd
+echo mmmmmmmmmmmmmmmmmNNNNNNhysoossyhdmmddmmNNmyydhyyyysssssooooosssyyhdddmmmddhyysysssydNNNNNNmmmmmmmddd
+echo mmmmmmmmmmmmmmmmmdhdNNhysoooooooosysshmmds/+oss+osso++//:/+osyhhddmmdhhyyyyyyyyyyyydNNNNNNmmmmdddmmd
+echo mmmmmmmmmmmmmmmmmhyhmdysoo+++////+ooooooo++++ooo+ooo+/::--:/+syhyyyhhmmddhysssyhhhhdNNNNmmmmmmmmdddd
+echo mmmmmmmmmmmmmmmmmyssdysso++++///::://+++++++++++++++/::---::/osssysoomNNNNddmdhhhhddNNNNmmmmmmmmdddd
+echo mmmmmmmmmmmmmmmmdyyydysoo++//:::::::::::::////+++++//:----::/+osssoooyhddysydmmdhhhdNNNmmmmmmmmmdddd
+echo dmddddddddddddddddhmdysso++/:::-----::::::::///+////::----::/++oooooooossossssssysyhNNNmmmmmmmmmddmd
+echo mmdmmdddmdddddmddmmNmyysoo+//::::----::::::://++/////:::::://+++o++++++++++++++oosshmmmmmmmmmmmmdddd
+echo mmmmmmmmmmmmmmmmmmdhyhyssoo++//::::::::::::://+++/////:::::/++++o++//////::////+ossymmmmmmmmmmmmmddd
+echo NNmmmmmmmmmmmmmmmmmhohyyssooo++//:::::::::://++++++//::::::/+++++++//////:::://++ssymmmmmmmmmmmmmddd
+echo NNmNNmmmmmmmmmmmmmmdyhhyyssooo++////::::://++oo+++//:::--:://++oo++////////////+osyhmmmmmmmmdmmmmmdd
+echo mmNNmmmmmmmmmmmmmmmmmmdhyyssooo+++////////+oo+++////::----://+++oo+/////////++oossydmmmmmmmmmmmmmddd
+echo mmmmmmmmmmmmmmmmmmmmmmmdhyysssooo+///////++oo+////:::-----://++++oo+/////+++oosssydmmmmmmmmmmmmmmddd
+echo mmmmmmmmmmmmmmmmmmmmmmmdhhyyssooo+++///+//+ssso++//::-----:://+++ooo+///+++oossyyhmmmmmddddmmmmmmmmd
+echo ssssyyhhhhddmmmmmmmmmmmdhhhyyssooo+++++////+syhdhso+////::://oosssooo++++ooossyhhdddmhhhhyyhhdddmmdd
+echo ooo+sssssssssyyyhhhhdddhhhhhyysssoo++++++///++osyyyysooo+++oyhhysooooo++ooossyhhhhyhhhdyyyssssyyhhdm
+echo ++o+osoooooosssyyyhyhyyyhhhhhhysssoo+++++++++++oooosyyhhyyyyyssooooooo+oossyyyhhyyyyysshdysssssssosy
+echo +++++yoooooooosysshhhdyyyhhhhhyyssooooooooo++oooooooosssssssssoooooooooossyyhhhhyyyyysssyhysssssosss
+echo +++++yoo+oooooossohyydyyyhhhhhyyysssssssssssoooooo+++ooooosssssooooooossyyhhhhhyssyyssssyysyoosoosys
+echo +++++so++oooooooooyyyhhsyhhhhhhyyyyyyyyhhysssooo++++ooo+++osssssssssssyyhhhyyhhysssssssosysyooooosyo
+echo ++++/so+++ooooooooshyyhoyysyhhhhhhhhhdhhhhyyyyssso++++///+ooossssyyyyyyhhhysyyhssssssssooyyyooooosyo
+echo ++++/so+++oooooo++ohyshoyyssyhhhhhhhyyssssssyyhdddhyssssyhhhhyyhhhhhhhhhhyssyhysssssssoooshoooooosyo
+echo ++++/so++++++o++++oyysyssysoshhhdhyyssssssssooossyyyhhhhhhhhhdhhhddddhdhysssyyyoosssoooooyyoooo++oyo
+echo /////oo+++++++++++oshsssoyssshhdddhyyyssyysssso++++ooooooossyyyyyhddddhsssssyysooooooooosysoooo++oyo
+echo /////oo++++++++++++syysyosysshhddddhyyyyyyyyyyyssooooo++oosyyyyyhhdddhsssssyyysooooooooossooooo++sso
+echo /////oo++++++++++++osysyoosssyhhddddhyyyyyyhhhddhhyyyyyyyyyhhhhhddddhssssssyyssooooooooosoooooo++sos
+echo ///+/oo++++++++++++osysyooossshyyhddhhyyyyyhhhhdddddddddddhhhhhhddmdyssssssyssooooooooossoooooo+osos
+echo /////oo++++++++++++oosyyoooossyyyyhdddhysyyyyyyyhhhhhhhhhhhyyhdddhmhssssosssssooooooooossoooooo+ooos
+echo ///+/oo++++++++++++oosyyoooosysyyyyyhdddhysssssyyyyyyyhhhyyhhddhyhmysssoossssoooooooo+osooooo+o+ooos
+echo ....................................................................................................
+echo Author: Siyabonga Gregory
+echo Email :huge.fuze@gnail.com
+set startTimer=%time%
+set countDownTimer=%150%
+:loop
+
+set /a countDownTimer=%countDownTimer%-1
+if %countDownTimer%==0 goto resertTimer
+
+ping localhost -n 2 > nul
+goto loop
+
+:resertTimer
+time %startTimer%
+goto start
